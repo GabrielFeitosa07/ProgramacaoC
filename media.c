12079013f21b53601c0d8ea18d6bd3e5fa27595e
Gabriel Feitosa <gabrielmfeitosa07@gmail.com>
2025-03-20 01:22:20 -0300

    Media
    Cálculo da média de 3 números

--- media.c
+++ media.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+
+float a,b,c;
+
+printf("Digite três números:\n");
+scanf("%f %f %f", &a,&b,&c);
+printf("A média dos números %.2f, %.2f, %.2f é igual a %.2f", a,b,c, (a+b+c)/3);
+
+return 0;
+
+}
